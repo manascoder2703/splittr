08e2b69a2d7880fbc23834664196f2ad421c42f7
manascoder2703 <manasmishra2703@gmail.com>
2025-07-21 00:32:49 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return <div>Subscribe to Mishrajigaming
+    <br/>
+    <Button variant={"destructive"}>Subscribe</Button>
+  </div>
+}
