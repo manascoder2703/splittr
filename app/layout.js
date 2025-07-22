7bd4f449610e1c64f04febf7b2d5456bdd69cc0e
manascoder2703 <manasmishra2703@gmail.com>
2025-07-22 20:05:29 +0530

    added convex client

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 const inter=Inter({subsets:["latin"]});
 
@@ -15,11 +16,12 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any"/>
       </head>
-      <body
-        className={'${inter.className}'}>
+      <body className={'${inter.className}'}>
+      <ConvexClientProvider>
         <Header/>
 
        <main className="min-h-screen"> {children}</main>
+       </ConvexClientProvider>
       </body>
     </html>
   );
