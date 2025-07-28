ebd87953b07ae5aeec0253dd028db00478cb326f
manascoder2703 <manasmishra2703@gmail.com>
2025-07-29 02:10:50 +0530

    some changes

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { SignedOut,SignInButton,SignUpButton,UserButton,SignedIn} from "@clerk/nextjs";
 const Header=()=>{
-    return <div>Header</div>;
+    return <div>
+        <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </div>;
 };
 
 export default Header;
