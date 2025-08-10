45e14bb55f133ba6bf3a6f9649d255d9ca9445c2
manascoder2703 <manasmishra2703@gmail.com>
2025-08-10 16:06:01 +0530

    designed landing page

--- components/header.jsx
+++ components/header.jsx
@@ -12,20 +12,21 @@ import { usePathname } from "next/navigation";
 import { Unauthenticated } from "convex/react";
 import { Button } from "./ui/button";
 import { Authenticated } from "convex/react";
+import { LayoutDashboard } from "lucide-react";
 const Header=()=>{
     const {isLoading}=useStoreUser();
     const path=usePathname();
     return( <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60" >
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between"> 
         <Link href="/" className="flex items-center gap-2">
-        <Image
-           src={"/logos/logo.png"}
-           alt="Splitr Logo"
-           width={200}
-           height={60}
-           className="h-11 w-auto object-contain"
-        />
-        </Link>
+  <Image
+    src="/logos/logo8.png" // new transparent version
+    alt="Splittr Logo"
+    width={300} // smaller width for balance
+    height={60}
+    className="h-10 w-auto object-contain"
+  />
+</Link>
 
         {path === '/' &&(
             <div className="hidden md:flex items-center gap-6">
@@ -47,8 +48,16 @@ const Header=()=>{
         <div className="flex items-center gap-4">
             <Authenticated>
                 <Link href="/dashboard">
-         
+                <Button variant="outline" className="hidden md:inline-flex items-center gap-2 hover:text-green-600 hover:border-green-600 transition">
+               <LayoutDashboard className="h-4 w-4" />
+                  Dashboard
+                  </Button>
+
+                  <Button variant="ghost" className="md:hidden w-10 h-10 p-0">
+               <LayoutDashboard className="h-4 w-4" />
+                  </Button>
                 </Link>
+                <UserButton/>
             </Authenticated>
             <Unauthenticated>
                 <SignInButton>
